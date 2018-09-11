96477c258c64765126cc0e36859eb83c905e4492
liubingqian <1140353932@qq.com>
2018-09-11 08:20:48 +0800

    homework1

---
+++ homework1/Program1/Program1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double a,b;
+            Console.Write("Please input two number:");
+            s = Console.ReadLine();
+            a = double.Parse(s);
+            s = Console.ReadLine();
+            b = double.Parse(s);
+            Console.WriteLine("a*b=" + a * b);
+        }
+    }
+}
---
+++ homework1/Program2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a1 = textBox1.Text;
+            string a2 = textBox2.Text;
+            double b1 = double.Parse(a1);
+            double b2 = double.Parse(a2);
+            double c = b1 * b2;
+            textBox3.Text = ""+c;
+        }
+
+
+    }
+}
